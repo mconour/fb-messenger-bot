cc8c7136650ca88241027e131ec116c8b776e3dd
Michael Conour <michaelconour@gmail.com>
2020-09-21 17:41:40 -0700

    init all web routes && use body-parser to post data

--- src/server.js
+++ src/server.js
@@ -5,5 +5,12 @@ import bodyParser from "body-parser";
 
 let app = express();
 
+// body-parser to post data
+app.arguments(bodyParser.json());
+app.arguments(bodyParser.urlencoded({ extended: true }));
+
 // config view engine
-configViewEngine(app0);
+configViewEngine(app);
+
+// init all web routes
+initWebRoutes(app);
