b0dc7361eead12e0ceedcdf5ba2486170eb86db0
Michael Conour <michaelconour@gmail.com>
2020-09-23 20:11:33 -0700

    update webhook

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -1,3 +1,7 @@
+require("dotenv").config();
+
+const MY_VERIFY_TOKEN = process.env.MY_VERIFY_TOKEN;
+
 let postWebhook = (req, res) => {
     let body = req.body;
 
@@ -22,7 +26,29 @@ let postWebhook = (req, res) => {
 };
 
 let getWebhook = (req, res) => {
+    // Your verify token. Should be a random string.
+    let VERIFY_TOKEN = MY_VERIFY_TOKEN;
 
+    // Parse the query params
+    let mode = req.query['hub.mode'];
+    let token = req.query['hub.verify_token'];
+    let challenge = req.query['hub.challenge'];
+
+    // Checks if a token and mode is in the query string of the request
+    if (mode && token) {
+
+        // Checks the mode and token sent is correct
+        if (mode === 'subscribe' && token === VERIFY_TOKEN) {
+
+            // Responds with the challenge token from the request
+            console.log('WEBHOOK_VERIFIED');
+            res.status(200).send(challenge);
+
+        } else {
+            // Responds with '403 Forbidden' if verify tokens do not match
+            res.sendStatus(403);
+        }
+    }
 };
 
 module.exports = {
