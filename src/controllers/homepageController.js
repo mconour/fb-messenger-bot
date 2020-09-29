007444437eaac79f528a6b6db6cc5b7474f27c16
Michael Conour <michaelconour@gmail.com>
2020-09-29 16:14:01 -0700

    fix server error && add the get started button and the persistent menu

--- src/controllers/homepageController.js
+++ src/controllers/homepageController.js
@@ -12,8 +12,58 @@ let getFacebookUserProfile = (req, res) => {
     return res.render("profile.ejs");
 };
 
-let setUpUserFacebookProfile = (req, res) => {       
-    
+let setUpUserFacebookProfile = (req, res) => {  
+       // Send the HTTP request to the Messenger Platform
+       let data = {
+        "get_started":{
+            "payload":"GET_STARTED"
+          },
+          "persistent_menu": [
+            {
+                "locale": "default",
+                "composer_input_disabled": false,
+                "call_to_actions": [
+                    {
+                        "type": "postback",
+                        "title": "Talk to an agent",
+                        "payload": "CARE_HELP"
+                    },
+                    {
+                        "type": "postback",
+                        "title": "Outfit suggestions",
+                        "payload": "CURATION"
+                    },
+                    {
+                        "type": "web_url",
+                        "title": "Shop now",
+                        "url": "https://www.originalcoastclothing.com/",
+                        "webview_height_ratio": "full"
+                    }
+                ]
+            }
+        ],
+        "whitelisted_domains":[
+            "https://chatbot-fb-messanger.herokuapp.com/"
+          ]
+       };
+
+  request({
+    "uri": "https://graph.facebook.com/v6.0/me/messenger_profile",
+    "qs": { "access_token": PAGE_ACCESS_TOKEN },
+    "method": "POST",
+    "json": data
+  }, (err, res, body) => {
+    if (!err) {
+      return res.status(200).json({
+          message: "setup complete"
+      })
+    } else {
+      return res.status(500).json({
+          "message": "Error from server"
+      })
+    }
+  });     
+
 
     return res.status(200).json({
         message: "OK"
