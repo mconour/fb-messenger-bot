5acb7020089f5b148bda7180728d2176ac920114
Michael Conour <michaelconour@gmail.com>
2020-10-01 11:48:09 -0700

    update init bot response in chat

--- src/controllers/chatBotController.js
+++ src/controllers/chatBotController.js
@@ -128,7 +128,7 @@ let handlePostback = async (sender_psid, received_postback) => {
         case "GET_STARTED":
             // get username
             let username = await chatBotService.getFacebookUsername(sender_psid);
-            response = {"text": `Hi, ${username}, welcome to RestaurantDemo!`};
+            response = {"text": `Hi, ${username}! Welcome to RestaurantDemo`};
             break;
         case "no":
             response = {};
